e8c25ff8ef22be23d241f7c170b806a1db7897d1
kureshiasiya <kureshiasiya2@gmail.com>
2023-10-13 20:50:49 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
+btnTranslate.addEventListener("click", clickHandler);
 
 function clickHandler() {
-  console.log("clicked!");
-  console.log("input", txtInput.value);
+  outputDiv.innerText = "ajsjsjsjsjs" + txtInput.value;
 }
-btnTranslate.addEventListener("click", clickHandler);
